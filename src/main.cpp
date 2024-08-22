be6d6390a752fb8150e9b81ba0375314299794f5
zahidulahaq <im.zahid182@gmail.com>
2024-08-23 01:32:04 +0530

    “hjfdh”

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,78 @@
 #include <iostream>
-int main()
+#include <string>
+#include <sstream>
+#include <filesystem>
+using namespace std;
+enum validCommands
 {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-  std::cout << "$ ";
-  std::string input;
- 
-  while (std::getline(std::cin, input) && input.find("exit") != 0)
-  {
-    if (input.find("echo ") == 0)
-    {
-      const int ECHO_LEN = 5; // Including space
-      std::string text = input.substr(ECHO_LEN);
-      std::cout << text << std::endl;
+    echo,
+    cd,
+    exit0,
+    type,
+    invalid,
+};
+validCommands isValid(std::string command){
+    command = command.substr(0,command.find(" "));
+    if(command == "echo") return validCommands::echo;
+    if(command == "cd") return validCommands::cd;
+    if(command == "exit") return validCommands::exit0;
+    if(command == "type") return validCommands::type;
+    return invalid;
+}
+std::string valid[4] = {"echo", "cd", "exit0"};
+std::string get_path(std::string command){
+    std::string path_env = std::getenv("PATH");
+    std::stringstream ss(path_env);
+    std::string path;
+    while(!ss.eof()){
+        getline(ss, path, ':');
+        string abs_path = path + '/' + command;
+        if(filesystem::exists(abs_path)){
+            return abs_path;
+        }
     }
-    else if(input.find("type ")==0){
-      if(input.substr(0,5) == "type "){
-      
-      std::string cmd = input.substr(5);
-      if(cmd.substr(0,4) == "type" || cmd.substr(0,4) == "exit" || cmd.substr(0,4) == "echo"){
-        std::cout << cmd << " is a shell builtin\n";
-      }else{
-        std::cout << cmd << " not found\n";
-      }
-   }
-
+    return "";  
+}
+int main() {
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    // std::cout << "Logs from your program will appear here!\n";
+    bool exit = false;
+    while (!exit){
+        // Flush after every std::cout / std:cerr
+        std::cout << std::unitbuf;
+        std::cerr << std::unitbuf;
+        std::cout << "$ ";
+        std::string input;
+        std::getline(std::cin, input);
+        switch(isValid(input)){
+            case cd:
+                break;
+            case echo:
+                input.erase(0,input.find(" ")+1);
+                std::cout<<input<<"\n";
+                break;
+            case exit0:
+                exit=true;
+                break;
+            case type:
+                input.erase(0,input.find(" ")+1);
+                if(isValid(input) != invalid){
+                    std::cout<<input<<" is a shell builtin\n";
+                }
+                else{
+                    std::cout<<input<<" not found\n";
+                    std::string path = get_path(input);
+                    if(path.empty()){
+                        std::cout<<input<<" not found\n";
+                    }
+                    else{
+                        std::cout<<input<<" is "<<path<<std::endl;
+                    }
+                }
+                break;
+            default:
+            std::cout<<input<<": command not found\n";
+                break;
+        }
     }
-    else
-    {
-      std::cout << input << ": command not found" << std::endl;
-    }
-    std::cout << "$ ";
-  }
-  exit(0);
 }
