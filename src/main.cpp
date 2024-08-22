68e3b6c3c73f25ad9aa3067d0a7c1a6ad239e4b7
zahidulahaq <im.zahid182@gmail.com>
2024-08-23 00:41:35 +0530

    kdfjdk

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::cout << "$ "<<endl;
+  std::cout << "$ ";
   
    std::string input;
    std::getline(std::cin, input);
