32484a5e0c3ab14feec214fa41b373bab67a8d0b
zahidulahaq <im.zahid182@gmail.com>
2024-08-23 01:10:58 +0530

    “hjfdh”

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,25 @@
 #include <iostream>
-
-int main() {
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
-   std::cout << "$ ";
-  
-   std::string input;
-   
-    
-    while (std::getline(std::cin, input) )
+  std::cout << "$ ";
+  std::string input;
+ 
+  while (std::getline(std::cin, input) && input.find("exit") != 0)
   {
-    if (input.find("echo ")==0)
+    if (input.find("echo ") == 0)
     {
       const int ECHO_LEN = 5; // Including space
       std::string text = input.substr(ECHO_LEN);
       std::cout << text << std::endl;
-      std::cout << "$ ";
-      
-    }
-    else if(input.find('exit'==0)){
-      return 0;
     }
     else
     {
-      
       std::cout << input << ": command not found" << std::endl;
-      std:: cout<<"$ ";
-    
     }
-  }
-  
-     while(true) {
     std::cout << "$ ";
-    std::string input;
-    
-    std::getline(std::cin, input);
-     
-   
-  
-    if(input == "exit 0") return 0;
-  //new
-    std::cout << input << ": command not found" << std::endl;
-     }
-   
-
+  }
+  exit(0);
 }
